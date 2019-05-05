9fbf19c8548b3c7cde5429598b6f6270c2047d2d
jerry <jerry@jerry-PC>
2019-05-05 16:13:47 +0800

    通知公告功能提交

---
+++ admin/src/main/resources/static/js/noticedetail.js
@@ -0,0 +1,44 @@
+/**
+* @author frank
+* @version 0.0.0(2019-03-25)
+*/
+
+$(function(){
+	// 初始化
+	sa_detail_init();
+	
+	// 初始化
+	function sa_detail_init(){
+		var id = GetQueryString("id");
+		var url = "";
+			url = "/API/notice/searchId/"+id;
+			
+		// 获取消息详情
+		$.ajax({
+			url: url,
+			type: 'GET',
+			data: {
+				
+			},
+			dataType: 'json',
+			success: function(resp){
+				$("#sa_detail_title").empty().html(resp.n_Title);
+				$("#sa_detail_content").empty().html(resp.n_Content);
+			},
+			error: function(err){
+				console.log(err);
+			}
+		});
+	}
+	
+	// 获取地址栏参数
+	function GetQueryString(name){
+		var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+		var r = window.location.search.substr(1).match(reg);
+		if(r!=null)return  unescape(r[2]);
+		return null;
+	}
+	
+});
+
+
---
+++ admin/src/main/resources/static/js/noticelist.js
@@ -0,0 +1,63 @@
+/**
+ * 新闻中心
+* @author frank
+* @version 0.0.0(2019-03-25)
+*/
+
+$(function(){
+	pageNum = 1
+	// 初始化
+	sa_list_init();
+	// 监听
+	myHandel();
+	
+	// 初始化
+	function sa_list_init(){
+			//新闻列表
+			$.getJSON("/API/notice/listPage",{'pageNum':pageNum,'pageSize':20},function(data){
+				var sa_list1_ele = $("#sa_list");
+				var sa_list1_str = "";
+				if(data.code == 0){
+					$.each(data.rows,function(index,value){
+						sa_list1_str += ''
+							+'<div class="row news-list" data-id="'+value.n_Id+'">'
+							+'	<div class="col-md-8 col-sm-8 col-xs-8" >'
+							+'		<div class="news-list-title">'+value.n_Title+'</div>'
+							+'	</div>'
+							+'	<div class="col-md-4 col-sm-4 col-xs-4" style="padding:0px;">'
+							+'		<div class="news-list-date">'+value.n_Date+'</div>'
+							+'	</div>'
+							+'</div>';
+					})
+					sa_list1_ele.empty();
+					sa_list1_ele.append(sa_list1_str);
+				}
+			})
+		}
+	
+		// 处理
+		function myHandel(){
+			// 消息点击
+			$("#sa_list").on("click",".news-list",function(ele){
+				var id = $(this).attr("data-id");
+				//跳转页面
+				if(id){
+					window.location.href = 'newsdetail.html?id='+id;
+				}else{
+					console.log("没有消息的id，请联系管理员");
+				}
+			});
+			
+		}
+	
+	// 获取地址栏参数
+	function GetQueryString(name){
+		var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+		var r = window.location.search.substr(1).match(reg);
+		if(r!=null)return  unescape(r[2]);
+		return null;
+	}
+	
+});
+
+
