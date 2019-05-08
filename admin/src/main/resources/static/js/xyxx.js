ac5305400524c3505c064137a146cbb2999bab09
jerry <jerry@jerry-PC>
2019-05-08 22:49:41 +0800

    学校信息

---
+++ admin/src/main/resources/static/js/xyxx.js
@@ -0,0 +1,45 @@
+/**
+* @author frank
+* @version 0.0.0(2019-03-25)
+*/
+
+$(function(){
+	// 初始化
+	sa_detail_init();
+	
+	// 初始化
+	function sa_detail_init(){
+		var schoolGuid = GetQueryString("schoolGuid");
+		var collegeGuid = GetQueryString("collegeGuid");
+		var url = "";
+			url = "/API/tjeducation/details?schoolGuid="+schoolGuid+"&collegeGuid="+collegeGuid;
+			
+		// 获取消息详情
+		$.ajax({
+			url: url,
+			type: 'GET',
+			data: {
+				
+			},
+			dataType: 'json',
+			success: function(resp){
+				$("#sa_detail_title").empty().html("学院简介");
+				$("#sa_detail_content").empty().html(resp.introduction);
+			},
+			error: function(err){
+				console.log(err);
+			}
+		});
+	}
+	
+	// 获取地址栏参数
+	function GetQueryString(name){
+		var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+		var r = window.location.search.substr(1).match(reg);
+		if(r!=null)return  unescape(r[2]);
+		return null;
+	}
+	
+});
+
+
